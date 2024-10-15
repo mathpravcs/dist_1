244826efdb795af34736b0dfb4733d813e24a990
mathpravcs <matheus_n11@live.com>
2024-10-15 14:50:34 -0300

    atualizacoes

---
+++ assets/index-636d542e.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-3da84cb3.js";function n(){return e.jsx("div",{children:e.jsx("h1",{children:"Teste seus componentes aqui"})})}export{n as default};
---
+++ assets/index-cd8d86c4.js
@@ -0,0 +1,1 @@
+import{aJ as o}from"./index-3da84cb3.js";const i=o;export{i as C};
